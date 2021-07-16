88d33f98da33f69b4e94ef38c0ce71d4f372bf68
Sean Spicer <sean.spicer@reveal-energy.com>
2021-07-16 10:37:43 -0500

    Create the text editor

---
+++ PythonnetTools/Pythonnet.Repl.Wpf/ConsoleControl.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows.Controls;
+
+namespace Pythonnet.Repl.Wpf
+{
+    public partial class ConsoleControl
+    {
+        public ConsoleControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
